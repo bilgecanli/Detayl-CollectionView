e6d0d098fadc2fa4161ce2f3866dc7c16140c80c
mehmet <mehmetsaidcanli@gmail.com>
2023-11-08 09:56:40 +0300

    Initial Commit

---
+++ DetaylıCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DetaylıCollectionView
+//
+//  Created by Mehmet Said Canlı on 8.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
